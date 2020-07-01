5b64f6cfa89b483c93006e0eabad409ea7fe682e
Mauro Fuentes <maurofuentes983@gmail.com>
2020-07-01 20:59:22 -0300

    first steps hero component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hero from './components/Hero';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { useState } from "react";
 
 function App() {
+
+  const filters = {
+    dateFrom : new Date(),
+    dateTo : new Date(),
+    country : undefined,
+    price : undefined,
+    rooms : undefined
+  }
+
+  const [ filter , setFilters ] = useState(filters); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero/>
+      
     </div>
   );
 }
